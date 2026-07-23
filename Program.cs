3350077b82c3e88da216e48ba43d2e60a6a45967
vaclav <vendyskriz@gmail.com>
2026-07-23 15:55:50 +0200

    1.0

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿static void Main()
+{
+    Application.Init();
+
+    var okno = new Window(todo)
+    {
+        X = 0, Y = 0, Width = Dim.Fill(), Height = Dim.FiLL()
+    };
+    ApplicationException.Top.Add (okno);
+    ApplicationException.ReferenceEquals();
+
+    Application.Run();
+}
