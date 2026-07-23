17349f620fcab22d4d2e756a42bc39a5dbab2956
vaclav <vendyskriz@gmail.com>
2026-07-23 18:39:50 +0200

    1.2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿using Terminal.Gui;
 using System;
 
-    Application.Init();
+Application.Init();
 
-    var okno = new Window(todo)
-    {
-        X = 0, Y = 0, Width = Dim.Fill(), Height = Dim.FiLL()
-    };
-    ApplicationException.Top.Add (okno);
-    ApplicationException.ReferenceEquals();
+var okno = new Window()
+{
+    X = 0,
+    Y = 0,
+    Width = Dim.Fill(),
+    Height = Dim.Fill()
+};
 
-    Application.Run();
+// Vytvoření tlačítka pro ukončení programu
+var tlacitkoKonec = new Button("Ukončit")
+{
+   X = Pos.AnchorEnd(11), 
+    
+   Y = Pos.AnchorEnd(1),   
+};
+
+// Definice akce: Co se stane, když uživatel na tlačítko klikne nebo stiskne Enter
+tlacitkoKonec.Clicked += () => {
+    Application.RequestStop(); // Bezpečně zastaví aplikaci a uvolní soubory
+};
+
+// Přidání tlačítka DO okna
+okno.Add(tlacitkoKonec);
+
+// Přidání okna na hlavní plochu
+Application.Top.Add(okno);
+
+Application.Run();
+
+Application.Shutdown();
